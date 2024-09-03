0b9bee22d428294e3b28b2028036d8a9fc7433bd
Alexander Garnica <131830717+Dantiann@users.noreply.github.com>
2024-09-03 15:30:06 -0500

    Add files via upload

---
+++ VerificacionEdad/VerificacionEdad/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace VerificacionEdad
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese el nombre: ");
+            var name = Console.ReadLine();
+            Console.WriteLine("Ingrese el año de nacimiento: ");
+            var Birthday = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Name: " + name + ". Birhday: " + Birthday);
+
+            Console.WriteLine("***************");
+
+            if (Birthday <= 2005)
+            {
+                Console.WriteLine(name + ", nacidO(a) en " + Birthday + " !ES APTO(A) PARA VOTAR¡");
+            }
+            else
+            {
+                Console.WriteLine(name + ", nacid(a) en " + Birthday + " !NO ES APTO(A) PARA VOTAR¡");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
